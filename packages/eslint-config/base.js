a2759739ab50881c0659cf426fd614d02b8b9a38
Muhammad Swalah A A <mail@swalahamani.com>
2025-11-23 11:42:40 +0530

    chore(root): remove unnecessary files and indent with tabs

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -10,23 +10,23 @@ import onlyWarn from "eslint-plugin-only-warn";
  * @type {import("eslint").Linter.Config[]}
  * */
 export const config = [
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      turbo: turboPlugin,
+    js.configs.recommended,
+    eslintConfigPrettier,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            turbo: turboPlugin,
+        },
+        rules: {
+            "turbo/no-undeclared-env-vars": "warn",
+        },
     },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
+    {
+        plugins: {
+            onlyWarn,
+        },
     },
-  },
-  {
-    plugins: {
-      onlyWarn,
+    {
+        ignores: ["dist/**"],
     },
-  },
-  {
-    ignores: ["dist/**"],
-  },
 ];
--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -14,44 +14,44 @@ import { config as baseConfig } from "./base.js";
  * @type {import("eslint").Linter.Config[]}
  * */
 export const nextJsConfig = [
-  ...baseConfig,
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-  {
-    ...pluginReact.configs.flat.recommended,
-    languageOptions: {
-      ...pluginReact.configs.flat.recommended.languageOptions,
-      globals: {
-        ...globals.serviceworker,
-      },
+    ...baseConfig,
+    js.configs.recommended,
+    eslintConfigPrettier,
+    ...tseslint.configs.recommended,
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
+    {
+        ...pluginReact.configs.flat.recommended,
+        languageOptions: {
+            ...pluginReact.configs.flat.recommended.languageOptions,
+            globals: {
+                ...globals.serviceworker,
+            },
+        },
     },
-  },
-  {
-    plugins: {
-      "@next/next": pluginNext,
+    {
+        plugins: {
+            "@next/next": pluginNext,
+        },
+        rules: {
+            ...pluginNext.configs.recommended.rules,
+            ...pluginNext.configs["core-web-vitals"].rules,
+        },
     },
-    rules: {
-      ...pluginNext.configs.recommended.rules,
-      ...pluginNext.configs["core-web-vitals"].rules,
+    {
+        plugins: {
+            "react-hooks": pluginReactHooks,
+        },
+        settings: { react: { version: "detect" } },
+        rules: {
+            ...pluginReactHooks.configs.recommended.rules,
+            // React scope no longer necessary with new JSX transform.
+            "react/react-in-jsx-scope": "off",
+        },
     },
-  },
-  {
-    plugins: {
-      "react-hooks": pluginReactHooks,
-    },
-    settings: { react: { version: "detect" } },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
-    },
-  },
 ];
